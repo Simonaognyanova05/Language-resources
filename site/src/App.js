b324412a548020815616037d5ea4aecce66782c9
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-02-13 18:16:03 +0200

    export components

--- site/src/App.js
+++ site/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import Products from "./components/Products";
+import Testimonial from "./components/Testimonial";
+import Info from "./components/Info";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/testimonial" element={<Testimonial />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
