49d87d67daa085680d4965fe93f91905ede14027
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-02-21 13:33:13 +0200

    add new colors in header

--- site/src/components/Header/Header.js
+++ site/src/components/Header/Header.js
@@ -10,21 +10,21 @@ export default function Header() {
     const userHead = (
         <>
             <li class="nav-item">
-                <Link class="nav-link" to="/about">За нас </Link>
+                <Link class="nav-link" to="/about" style={{ color: "black" }}>За нас </Link>
             </li>
 
             <li class="nav-item">
-                <Link class="nav-link" to="/contacts">Контакти</Link>
+                <Link class="nav-link" to="/contacts" style={{ color: "black" }}>Контакти</Link>
             </li>
         </>
     );
     const unLoggedUser = (
         <>
             <li class="nav-item">
-                <Link class="nav-link" to="/login">Влизане</Link>
+                <Link class="nav-link" to="/login" style={{ color: "black" }}>Влизане</Link>
             </li>
             <li class="nav-item">
-                <Link class="nav-link" to="/register">Регистрация</Link>
+                <Link class="nav-link" to="/register" style={{ color: "black" }}>Регистрация</Link>
             </li>
         </>
     );
@@ -32,13 +32,13 @@ export default function Header() {
     const loggedUser = (
         <>
             <li class="nav-item">
-                <Link class="nav-link" to="/userProfile">Моят профил</Link>
+                <Link class="nav-link" to="/userProfile" style={{ color: "black" }}>Моят профил</Link>
             </li>
             <li class="nav-item">
-                <Link class="nav-link" to="/logout">Изход</Link>
+                <Link class="nav-link" to="/logout" style={{ color: "black" }}>Изход</Link>
             </li>
             <li className="nav-item">
-                <Link to="/cart" className="nav-link cart-link">
+                <Link to="/cart" className="nav-link cart-link" style={{ color: "black" }}>
                     <i className="bi bi-cart3"></i>
                 </Link>
             </li>
@@ -50,10 +50,10 @@ export default function Header() {
     const admin = (
         <>
             <li class="nav-item">
-                <Link class="nav-link" to="/createProduct">Създай продукт</Link>
+                <Link class="nav-link" to="/createProduct" style={{ color: "black" }}>Създай продукт</Link>
             </li>
             <li class="nav-item">
-                <Link class="nav-link" to="/orders">Поръчки</Link>
+                <Link class="nav-link" to="/orders" style={{ color: "black" }}>Поръчки</Link>
             </li>
         </>
     );
@@ -63,7 +63,7 @@ export default function Header() {
                 <div class="brand_box">
                     <a class="navbar-brand" href="index.html">
                         <span>
-                            Ninom
+                            Учебен център Варна
                         </span>
                     </a>
                 </div>
@@ -72,24 +72,24 @@ export default function Header() {
                         <div class="carousel-inner">
                             <div class="carousel-item active">
                                 <div class="img-box">
-                                    <img src="images/slider-img.jpg" alt="" />
+                                    <img src="images/img1.jpg" alt="" />
                                 </div>
                             </div>
                             <div class="carousel-item">
                                 <div class="img-box">
-                                    <img src="images/slider-img.jpg" alt="" />
+                                    <img src="images/img2.jpg" alt="" />
                                 </div>
                             </div>
                             <div class="carousel-item">
                                 <div class="img-box">
-                                    <img src="images/slider-img.jpg" alt="" />
+                                    <img src="images/img3.jpg" alt="" />
                                 </div>
                             </div>
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style={{ backgroundColor: "#B21F7A" }}>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style={{ backgroundColor: "#B21F7A" }}>
                             <span class="sr-only">Next</span>
                         </a>
                     </div>
@@ -100,19 +100,19 @@ export default function Header() {
             <section class="nav_section">
                 <div class="container">
                     <div class="custom_nav2">
-                        <nav class="navbar navbar-expand custom_nav-container ">
+                        <nav class="navbar navbar-expand custom_nav-container " style={{ backgroundColor: "#b9b1cc" }}>
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
 
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent" >
                                 <div class="d-flex  flex-column flex-lg-row align-items-center">
-                                    <ul class="navbar-nav  ">
-                                        <li class="nav-item active">
-                                            <Link class="nav-link" to="/">Начало <span class="sr-only">(current)</span></Link>
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item active" >
+                                            <Link class="nav-link" to="/" style={{ color: "black" }}>Начало <span class="sr-only">(current)</span></Link>
                                         </li>
                                         <li class="nav-item">
-                                            <Link class="nav-link" to="/products">Продукти </Link>
+                                            <Link class="nav-link" to="/products" style={{ color: "black" }}>Продукти </Link>
                                         </li>
 
                                         {isAdmin(user.email) ? admin : userHead}
