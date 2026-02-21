7bf1135910ef7784a89a36424a7f4bbba10962ea
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-02-21 14:26:29 +0200

    add new design for about

--- site/src/components/About.js
+++ site/src/components/About.js
@@ -1,29 +1,57 @@
 export default function About() {
     return (
-        <section class="about_section">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-6 px-0">
-                        <div class="img-box">
-                            <img src="images/about-img.jpg" alt="" />
-                        </div>
+        <section
+            className="py-5"
+            style={{
+                background: "linear-gradient(180deg, #f8f9fa 50%, #dcd0ea 50%)"
+            }}
+        >
+            <div className="container">
+                <div className="row align-items-center">
+
+                    {/* Text Content */}
+                    <div className="col-lg-6 mb-4 mb-lg-0">
+                        <h1 className="fw-bold display-5 mb-4">
+                            По 21 езика в Учебен център Варна
+                        </h1>
+
+                        <p className="lead text-muted mb-4">
+                            Удобни графици, съобразени с вашето време.
+                        </p>
+
+                        <p style={{ lineHeight: "1.8" }}>
+                            През 2006 г. започнахме пътуването си с една цел – да направим
+                            езиковото обучение достъпно, ефективно и забавно за всички
+                            възрасти. Присъединете се към нас и подобрете езиковите си
+                            умения по ваш избор – с гъвкави курсове в групи или
+                            индивидуално!
+                        </p>
+
+                        <button
+                            className="btn mt-3 px-4 py-2"
+                            style={{
+                                background: "linear-gradient(135deg, #B21F7A, #6A1B9A)",
+                                color: "white",
+                                borderRadius: "30px",
+                                border: "none"
+                            }}
+                        >
+                            Научи повече
+                        </button>
                     </div>
-                    <div class="col-md-5">
-                        <div class="detail-box">
-                            <div class="heading_container">
-                                <hr />
-                                <h2>
-                                    About Our Fruit Shop
-                                </h2>
-                            </div>
-                            <p>
-                                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour
-                            </p>
-                            <a href="">
-                                Read More
-                            </a>
-                        </div>
+
+                    {/* Image */}
+                    <div className="col-lg-6 text-center">
+                        <img
+                            src="/images/about-img.jpg"
+                            alt="about"
+                            className="img-fluid"
+                            style={{
+                                maxHeight: "420px"
+                            }}
+                        />
                     </div>
+
                 </div>
             </div>
         </section>
