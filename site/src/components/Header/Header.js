e75ed2856245987f7421abb8a94aba3cb5027c46
Simonaognyanova05 <simonaognanova05@gmail.com>
2026-02-23 13:27:11 +0200

    add hamburger for small devices

--- site/src/components/Header/Header.js
+++ site/src/components/Header/Header.js
@@ -1,5 +1,8 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './Header.css';
+
 import { Link } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import { isAdmin } from "../../services/isAdmin";
@@ -9,119 +12,148 @@ export default function Header() {
 
     const userHead = (
         <>
-            <li class="nav-item">
-                <Link class="nav-link" to="/about" style={{ color: "black" }}>За нас </Link>
+            <li className="nav-item">
+                <Link className="nav-link" to="/about">За нас</Link>
             </li>
 
-            <li class="nav-item">
-                <Link class="nav-link" to="/contacts" style={{ color: "black" }}>Контакти</Link>
+            <li className="nav-item">
+                <Link className="nav-link" to="/contacts">Контакти</Link>
             </li>
         </>
     );
+
     const unLoggedUser = (
         <>
-            <li class="nav-item">
-                <Link class="nav-link" to="/login" style={{ color: "black" }}>Влизане</Link>
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">Влизане</Link>
             </li>
-            <li class="nav-item">
-                <Link class="nav-link" to="/register" style={{ color: "black" }}>Регистрация</Link>
+            <li className="nav-item">
+                <Link className="nav-link" to="/register">Регистрация</Link>
             </li>
         </>
     );
 
     const loggedUser = (
         <>
-            <li class="nav-item">
-                <Link class="nav-link" to="/userProfile" style={{ color: "black" }}>Моят профил</Link>
-            </li>
-            <li class="nav-item">
-                <Link class="nav-link" to="/logout" style={{ color: "black" }}>Изход</Link>
-            </li>
             <li className="nav-item">
-                <Link to="/cart" className="nav-link cart-link" style={{ color: "black" }}>
+                <Link className="nav-link" to="/userProfile">Моят профил</Link>
+            </li>
+
+            <li className="nav-item">
+                <Link className="nav-link" to="/logout">Изход</Link>
+            </li>
+
+            <li className="nav-item">
+                <Link to="/cart" className="nav-link cart-link">
                     <i className="bi bi-cart3"></i>
                 </Link>
             </li>
-
-
         </>
     );
 
     const admin = (
         <>
-            <li class="nav-item">
-                <Link class="nav-link" to="/createProduct" style={{ color: "black" }}>Създай продукт</Link>
+            <li className="nav-item">
+                <Link className="nav-link" to="/createProduct">Създай продукт</Link>
             </li>
-            <li class="nav-item">
-                <Link class="nav-link" to="/orders" style={{ color: "black" }}>Поръчки</Link>
+            <li className="nav-item">
+                <Link className="nav-link" to="/orders">Поръчки</Link>
             </li>
         </>
     );
+
     return (
         <>
-            <div class="hero_area">
-                <div class="brand_box">
-                    <a class="navbar-brand" href="index.html">
-                        <span>
-                            Учебен център
-                        </span>
-                    </a>
+            <div className="hero_area">
+
+                <div className="brand_box">
+                    <Link className="navbar-brand" to="/">
+                        <span>Учебен център</span>
+                    </Link>
                 </div>
-                <section class=" slider_section position-relative">
-                    <div id="carouselExampleControls" class="carousel slide " data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <div class="img-box">
+
+                {/* ===== КАРУСЕЛ (НЕ ПИПАМЕ) ===== */}
+                <section className="slider_section position-relative">
+                    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+
+                        <div className="carousel-inner">
+
+                            <div className="carousel-item active">
+                                <div className="img-box">
                                     <img src="images/img1.jpg" alt="" />
                                 </div>
                             </div>
-                            <div class="carousel-item">
-                                <div class="img-box">
+
+                            <div className="carousel-item">
+                                <div className="img-box">
                                     <img src="images/img2.jpg" alt="" />
                                 </div>
                             </div>
-                            <div class="carousel-item">
-                                <div class="img-box">
+
+                            <div className="carousel-item">
+                                <div className="img-box">
                                     <img src="images/img3.jpg" alt="" />
                                 </div>
                             </div>
+
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" style={{ backgroundColor: "#B21F7A" }}>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" style={{ backgroundColor: "#B21F7A" }}>
-                            <span class="sr-only">Next</span>
-                        </a>
+
+                        <button className="carousel-control-prev"
+                            type="button"
+                            data-bs-target="#carouselExampleControls"
+                            data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon"></span>
+                        </button>
+
+                        <button className="carousel-control-next"
+                            type="button"
+                            data-bs-target="#carouselExampleControls"
+                            data-bs-slide="next">
+                            <span className="carousel-control-next-icon"></span>
+                        </button>
+
                     </div>
                 </section>
             </div>
 
+            {/* ===== NAVBAR ===== */}
+            <section className="nav_section">
+                <div className="container">
 
-            <section class="nav_section">
-                <div class="container">
-                    <div class="custom_nav2">
-                        <nav class="navbar navbar-expand custom_nav-container " style={{ backgroundColor: "#b9b1cc" }}>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
+                    <nav className="navbar navbar-expand-lg custom_nav-container">
 
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-                                <div class="d-flex  flex-column flex-lg-row align-items-center">
-                                    <ul class="navbar-nav">
-                                        <li class="nav-item active" >
-                                            <Link class="nav-link" to="/" style={{ color: "black" }}>Начало <span class="sr-only">(current)</span></Link>
-                                        </li>
-                                        <li class="nav-item">
-                                            <Link class="nav-link" to="/products" style={{ color: "black" }}>Продукти </Link>
-                                        </li>
+                        {/* HAMBURGER */}
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
-                                        {isAdmin(user.email) ? admin : userHead}
-                                        {user.email ? loggedUser : unLoggedUser}
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
+                        {/* MENU */}
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+                            <ul className="navbar-nav mx-auto">
+
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/">Начало</Link>
+                                </li>
+
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/products">Продукти</Link>
+                                </li>
+
+                                {user?.email && isAdmin(user.email) ? admin : userHead}
+                                {user?.email ? loggedUser : unLoggedUser}
+
+                            </ul>
+
+                        </div>
+
+                    </nav>
+
                 </div>
             </section>
         </>
